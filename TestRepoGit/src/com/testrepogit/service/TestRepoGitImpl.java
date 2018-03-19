2a0710698b38a63dfe658fe8060481325a736d95
pravat <pravat@192.168.2.4>
2018-03-19 23:47:16 +0530

    infant check functionality added

--- TestRepoGit/src/com/testrepogit/service/TestRepoGitImpl.java
+++ TestRepoGit/src/com/testrepogit/service/TestRepoGitImpl.java
@@ -1,11 +1,28 @@
 package com.testrepogit.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class TestRepoGitImpl {
  private static final String dateFormat = "dd/MM/yyyyy";
- 
+ private static final SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
  public boolean isSeniorCitizen(Date dob) {
 	 return false;
+ }
+ 
+ public boolean isInfant(String dobOfinfant) throws ParseException {
+	Date dob  = sdf.parse(dobOfinfant);
+	Date currentDate = new Date();
+	Long diff = dob.getTime() - currentDate.getTime();
+	long days = TimeUnit.DAYS.convert(diff, TimeUnit.MICROSECONDS);
+	int years = (int) (days/12);
+	if(years < 3) {
+		return true;
+	}
+	return false;
+	 
  }
 }
