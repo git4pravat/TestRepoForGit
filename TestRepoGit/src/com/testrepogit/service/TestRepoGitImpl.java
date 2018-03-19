261159010f27d8ecca9bd6e5e42b88f4b498a13b
pravat <pravat@192.168.2.4>
2018-03-19 23:28:04 +0530

    senior citizen check added

--- TestRepoGit/src/com/testrepogit/service/TestRepoGitImpl.java
+++ TestRepoGit/src/com/testrepogit/service/TestRepoGitImpl.java
@@ -1,5 +1,11 @@
 package com.testrepogit.service;
 
+import java.util.Date;
+
 public class TestRepoGitImpl {
  private static final String dateFormat = "dd/MM/yyyyy";
+ 
+ public boolean isSeniorCitizen(Date dob) {
+	 return false;
+ }
 }
